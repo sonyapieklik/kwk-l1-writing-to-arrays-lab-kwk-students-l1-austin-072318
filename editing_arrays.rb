95e1fb60f0c6aa039ebeb97fb934103618b8709e
sonyapieklik <piesonya@gmail.com>
2018-07-24 18:56:15 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -9,6 +9,7 @@ def change_rainbow_colors
 end
 
 def add_colors
-  rainbow_colors << "green"
-  rainbow_colors << "blue"
+  rainbow_colors = ['red', 'light_red', 'light_yellow']
+  rainbow_colors.push "green"
+  rainbow_colors.push "blue"
 end
